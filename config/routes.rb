7ffa9e10f0b812ef4e0396481defa4091a0cba55
tan nguyen <saigoncuoinam@gmail.com>
2023-03-07 16:21:26 +1100

    bookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :trips, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:create]
   end
+  resources :bookings, only: [:show, :index, :edit]
 end

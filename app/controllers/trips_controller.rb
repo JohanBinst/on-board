429cc5c947620fe0f52918b9e311089dd5b3df57
Johan Binst <johan.binst@gmail.com>
2023-03-07 13:12:23 +1100

    added my trips view and trip delete function

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -15,4 +15,17 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
   end
+
+  def my_trips
+    # @user = current_user
+    @trips = Trip.where(user_id: current_user.id)
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to my_trips_path(current_user), status: :see_other
+  end
+
+
 end

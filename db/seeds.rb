70ed35a92287a3579dfb9a87011f8e4d08fcfc19
Johan Binst <johan.binst@gmail.com>
2023-03-16 16:36:19 +1100

    login fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,7 +59,7 @@ def create_destination
 end
 
 puts "Creating 10 trips ..."
-10.times do |i|
+20.times do |i|
   # photo = URI.open("https://source.unsplash.com/random/1920x1080/?#{activity}")
   user = User.all.sample
   departure_point = Trip::DEPARTURE.keys.sample

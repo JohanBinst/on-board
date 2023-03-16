47e8c9d4461bfa9a2786a9eeb5b705660dbee76c
Johan Binst <johan.binst@gmail.com>
2023-03-16 15:54:47 +1100

    changed seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -87,6 +87,7 @@ puts "Creating 10 trips ..."
  4.times do |n|
     puts "looking for photos, photo #{n + 1}/4"
     photo = URI.open("https://source.unsplash.com/random/640x480/?#{activity}")
+    sleep(2)
     blob = { io: photo, filename: "#{i}.jpg", content_type: "image/jpg" }
     blobs_array << blob
   end
